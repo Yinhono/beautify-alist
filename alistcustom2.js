52b4c59849f12763ec3e269f4a9ae0a4248f4194
Rainyin <115003789+Yinhono@users.noreply.github.com>
2024-11-05 21:38:34 +0800

    Update and rename alistcustom.js to alistcustom2.js

--- alistcustom2.js
+++ alistcustom2.js
@@ -1,27 +1,20 @@
-// 创建自定义内容
-const customContent = `
 <div id="customize" style="display: none;">
+    <!-- 自定义页脚内容 -->
     <div class="custom-footer" style="text-align: center;">
         <p align="center">
             &copy; Powered by alist
             <span>|</span>
-            <a target="_blank" href="https://blog.ling-yin.top/2024/11/03/mp/">服务条款</a>
+            <a target="_blank" href="https://blog.ling-yin.top/2024/11/03/mp/" >服务条款</a>
         </p>
         <p align="center">开放、平等、协作、共享 ——互联网精神</p>
     </div>
-</div>
-`;
 
-// 等待 DOM 加载完成后插入自定义内容
-document.addEventListener('DOMContentLoaded', function() {
-    // 将自定义内容插入到页面底部
-    document.body.insertAdjacentHTML('beforeend', customContent);
-
-    // 检查并显示自定义内容
-    const interval = setInterval(() => {
+<!--延迟加载配套使用JS-->
+<script>
+    let interval = setInterval(() => {
         if (document.querySelector(".footer")) {
             document.querySelector("#customize").style.display = "";
             clearInterval(interval);
         }
     }, 200);
-});
+</script>
