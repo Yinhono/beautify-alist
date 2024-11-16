d5d1b3081dbbf61b08ca9879217368c7b8159d77
Rainyin <115003789+Yinhono@users.noreply.github.com>
2024-11-16 11:20:55 +0800

    Update custom-footerv2.js

--- custom-footerv2.js
+++ custom-footerv2.js
@@ -1,3 +1,3 @@
-fetch('https://cdn.jsdelivr.net/gh/Yinhono/beautify-alist@latest/pagev3.html')
+fetch('https://cdn.jsdelivr.net/gh/Yinhono/beautify-alist@latest/pagev4.html')
         .then(response => response.text())
         .then(data => document.getElementById('custom-footer').innerHTML = data);
