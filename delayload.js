f4b503538554ad06f0017b0212e0cb4849656ca7
Rainyin <115003789+Yinhono@users.noreply.github.com>
2024-11-16 11:26:27 +0800

    Delete delayload.js

--- delayload.js
+++
@@ -1,9 +0,0 @@
-<!--延迟加载配套使用JS-->
-<script>
-    let interval = setInterval(() => {
-        if (document.querySelector(".footer")) {
-            document.querySelector("#customize").style.display = "";
-            clearInterval(interval);
-        }
-    }, 200);
-</script>
